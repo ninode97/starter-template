73ae9e399c5cb840e88ac96eb49934d3de1800a0
ninode <ninode97>
2023-08-05 10:35:04 +0300

    fix: updated auth dep

--- apps/auth/src/main.ts
+++ apps/auth/src/main.ts
@@ -1,22 +1,17 @@
-/**
- * This is not a production server yet!
- * This is only a minimal backend to get started.
- */
-
-import { Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
-
-import { AppModule } from './app/app.module';
+import { RmqService } from '@app/common';
+import { AuthModule } from './app/auth.module';
+import { RmqOptions } from '@nestjs/microservices';
+import { ValidationPipe } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  const globalPrefix = 'api';
-  app.setGlobalPrefix(globalPrefix);
-  const port = process.env.PORT || 3000;
-  await app.listen(port);
-  Logger.log(
-    `🚀 Application is running on: http://localhost:${port}/${globalPrefix}`
-  );
+  const app = await NestFactory.create(AuthModule);
+  const rmqService = app.get<RmqService>(RmqService);
+  app.connectMicroservice<RmqOptions>(rmqService.getOptions('AUTH', true));
+  app.useGlobalPipes(new ValidationPipe());
+  const configService = app.get(ConfigService);
+  await app.startAllMicroservices();
+  await app.listen(configService.get('PORT'));
 }
-
 bootstrap();
